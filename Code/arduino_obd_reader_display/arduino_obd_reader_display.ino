bcc578386424b5bf2b156a9510ea8d77b5f7cb89
Ravi Shah <ravidhruvishshah@gmail.com>
2024-06-04 23:56:50 -0500

    update reader code

--- Code/arduino_obd_reader_display/arduino_obd_reader_display.ino
+++ Code/arduino_obd_reader_display/arduino_obd_reader_display.ino
@@ -5,8 +5,8 @@
 
 #include "OBD9141.h"
 
-#define RX_PIN 8  // connect to transceiver Rx
-#define TX_PIN 9  // connect to transceiver Tx
+#define RX_PIN 8   // connect to transceiver Rx
+#define TX_PIN 9   // connect to transceiver Tx
 #define EN_PIN 10  //  pin will be set high (connect to EN pin of SN65HVDA100)
 
 AltSoftSerial altSerial;
@@ -14,53 +14,52 @@ AltSoftSerial altSerial;
 OBD9141 obd;
 
 
-void setup(){
-    Serial.begin(9600);
-    delay(2000);
-    pinMode(EN_PIN, OUTPUT);
-    digitalWrite(EN_PIN, HIGH); // enable the transceiver IC.
+void setup() {
+  Serial.begin(9600);
+  delay(2000);
+  pinMode(EN_PIN, OUTPUT);
+  digitalWrite(EN_PIN, HIGH);  // enable the transceiver IC.
 
-    obd.begin(altSerial, RX_PIN, TX_PIN);
-
+  obd.begin(altSerial, RX_PIN, TX_PIN);
 }
-    
-void loop(){
-    Serial.println("Looping");
 
-    bool init_success =  obd.init();
-    Serial.print("init_success:");
-    Serial.println(init_success);
+void loop() {
+  Serial.println("Looping");
 
-    //init_success = true;
-    // Uncomment this line if you use the simulator to force the init to be
-    // interpreted as successful. With an actual ECU; be sure that the init is 
-    // succesful before trying to request PID's.
+  bool init_success = obd.init();
+  Serial.print("init_success:");
+  Serial.println(init_success);
 
-    if (init_success){
-        bool res;
-        while(1){
-            res = obd.getCurrentPID(0x11, 1);
-            if (res){
-                Serial.print("Result 0x11 (throttle): ");
-                Serial.println(obd.readUint8());
-            }
-            
-            res = obd.getCurrentPID(0x0C, 2);
-            if (res){
-                Serial.print("Result 0x0C (RPM): ");
-                Serial.println(obd.readUint16()/4);
-            }
+  //init_success = true;
+  // Uncomment this line if you use the simulator to force the init to be
+  // interpreted as successful. With an actual ECU; be sure that the init is
+  // succesful before trying to request PID's.
+
+  if (init_success) {
+    bool res;
+    while (1) {
+      res = obd.getCurrentPID(0x11, 1);
+      if (res) {
+        Serial.print("Result 0x11 (throttle): ");
+        Serial.println(obd.readUint8());
+      }
+
+      res = obd.getCurrentPID(0x0C, 2);
+      if (res) {
+        Serial.print("Result 0x0C (RPM): ");
+        Serial.println(obd.readUint16() / 4);
+      }
 
 
-            res = obd.getCurrentPID(0x0D, 1);
-            if (res){
-                Serial.print("Result 0x0D (speed): ");
-                Serial.println(obd.readUint8());
-            }
-            Serial.println();
+      res = obd.getCurrentPID(0x0D, 1);
+      if (res) {
+        Serial.print("Result 0x0D (speed): ");
+        Serial.println(obd.readUint8());
+      }
+      Serial.println();
 
-            delay(200);
-        }
+      delay(200);
     }
-    delay(3000);
+  }
+  delay(3000);
 }
