71ddb1b97a5d21fef894fddf756faa991c0b5ca2
Ravi Shah <ravidhruvishshah@gmail.com>
2024-06-29 16:31:15 -0500

    add file shell for LED driver

---
+++ Code/arduino_obd_reader_display/LED_driver.ino
@@ -0,0 +1,41 @@
+// LED Driver file contains functions to display certain values using the 7 LEDs on the device
+
+// Startup sequence runs when device is connected and initializes a connection with the ECU
+// TODO: describe what happens here....
+void startup() {
+  
+}
+
+// TODO: describe the one-seven functions
+void one() {
+  
+}
+
+void two() {
+  
+}
+
+void three() {
+  
+}
+
+void four() {
+  
+}
+
+void five() {
+  
+}
+
+void six() {
+  
+}
+
+void seven() {
+  
+}
+
+// TODO: describe this
+void displayRPM(int RPM) {
+
+}
